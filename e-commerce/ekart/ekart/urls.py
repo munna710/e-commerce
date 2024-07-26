a319038e5af95953962bad451cf4396145bd2f5a
munna710 <munna7777777.1.0@gmail.com>
2024-07-26 22:05:59 +0530

    chore: Refactor customer and order views, and add registration and login functionality

--- e-commerce/ekart/ekart/urls.py
+++ e-commerce/ekart/ekart/urls.py
@@ -22,6 +22,9 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
+    path('customer/',include('customers.urls')),
+    path('orders/',include('orders.urls'))
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- e-commerce/ekart/products/views.py
+++ e-commerce/ekart/products/views.py
@@ -3,3 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html' )
+def list_products(request):
+    """_summary_
+    returns prouduct list page
+    Args:
+        request (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    
+    return render(request,'products.html')
+
+def detail_product(request,pk):
+    return render(request,'product_detail.html')
